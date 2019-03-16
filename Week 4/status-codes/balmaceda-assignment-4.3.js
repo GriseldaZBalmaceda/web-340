cb603a90592c7a522994ea0b22cfaeead6dcb685
Brian Gordon <gzbalmaceda@my365.bellevue.edu>
2019-03-15 20:13:05 -0600

    commiting assignmetn 4.3

---
+++ Week 4/status-codes/balmaceda-assignment-4.3.js
@@ -0,0 +1,31 @@
+//require needed modules
+var express = require("express");
+var http = require("http");
+var app = express();
+app.get("/not-found", function (request, response) {
+
+    //making the statuses and their response messages
+    response.status(404);
+    response.json({
+        error: "Page not found :("
+    });
+
+});
+
+app.get("/ok", function (request, response) {
+    response.status(200);
+    response.json({
+        message: "Page loaded :)"
+    })
+});
+
+app.get("/not-implemented", function (request, response) {
+    response.status(501);
+    response.json({
+        message: "Page not implemented"
+    })
+});
+
+http.createServer(app).listen(8080, function () {
+    console.log("Application started on port 8080")
+});
